6f164f70f9315871996d04e9ad0bfc9052c063f9
zaki tayyip <zakidayib1997@gmail.com>
2023-05-20 11:52:56 +0300

    largest of three solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let num1 = 5, num2 = 7, num3 = 2;
+
+if (num1 >num2 && num1 > num3){
+    console.log(`${num1} is the largest of three`);
+}
+else if (num2 >num1 && num2 > num3){
+    console.log(`${num2} is the largest of three`);
+}
+else{
+    console.log(`${num3} is the largest of three`);
+}
